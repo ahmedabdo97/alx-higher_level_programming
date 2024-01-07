5aca26c719fbbf4ad0943f35e42da2b3810617c2
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2024-01-07 12:20:52 -0800

    is lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
 
-if chr(c) >= 97 and chr(c) <= 122:
-    return True
-else:
-    return False
+    if chr(c) >= 97 and chr(c) <= 122:
+        return True
+    else:
+        return False
