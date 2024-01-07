d4499c1fb1e7d5450ac912d37820b94ee07cd029
Ahmed Mohamed Abdo <windwave41@yahoo.com>
2024-01-07 10:46:36 -0800

    2alphabet

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,9 @@
+for letter in range(97, 123):
+    q = chr(113)
+    e = chr(101)
+    letter_alp = chr(letter)
+    if letter_alp == q or letter_alp == e:
+        continue
+    else:
+        print("{}".format(letter_alp), end="")
+
